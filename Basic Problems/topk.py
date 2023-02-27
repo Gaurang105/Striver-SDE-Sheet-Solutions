3bcded6f08404f4de2755feac3c729d93ec64e6e
Gaurang105 <gujratigaurang@gmail.com>
2023-02-27 20:32:41 +0530

    sol update

--- Basic Problems/topk.py
+++ Basic Problems/topk.py
@@ -7,5 +7,4 @@ class Solution:
         freq_list = [(num, freq_dict[num]) for num in freq_dict]
 
         freq_list.sort(key = lambda x: (-x[1], -x[0]))
-
         return [freq_list[i][0] for i in range(k)]
